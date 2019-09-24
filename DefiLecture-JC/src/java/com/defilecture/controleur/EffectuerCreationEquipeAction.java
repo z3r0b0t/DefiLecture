c7dc00dc6a4e2be74a1fb62691a0deea0aa6872b
z3r0b0t <mikaelnadeau0@gmail.com>
2019-09-23 23:52:19 -0400

    Enlever ancien import CreationEquipe

--- DefiLecture-JC/src/java/com/defilecture/controleur/EffectuerCreationEquipeAction.java
+++ DefiLecture-JC/src/java/com/defilecture/controleur/EffectuerCreationEquipeAction.java
@@ -21,7 +21,6 @@ import com.defilecture.modele.DemandeEquipe;
 import com.defilecture.modele.DemandeEquipeDAO;
 import com.defilecture.modele.Equipe;
 import com.defilecture.modele.EquipeDAO;
-import com.util.Util;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
